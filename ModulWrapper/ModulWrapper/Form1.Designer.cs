6b888fe297bb0ccb1a83257ea4b1f7ea90f4afb1
toshabelof <shadow35rus@gmail.com>
2019-02-07 00:51:28 +0300

    060219

---
+++ ModulWrapper/ModulWrapper/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ModulWrapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBox_path = new System.Windows.Forms.TextBox();
+            this.btn_Detect = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tBox_path
+            // 
+            this.tBox_path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBox_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tBox_path.ForeColor = System.Drawing.Color.Gray;
+            this.tBox_path.Location = new System.Drawing.Point(12, 15);
+            this.tBox_path.Name = "tBox_path";
+            this.tBox_path.Size = new System.Drawing.Size(810, 20);
+            this.tBox_path.TabIndex = 1;
+            this.tBox_path.Text = "Enter the path to the photo and to press Enter key";
+            this.tBox_path.WordWrap = false;
+            this.tBox_path.Enter += new System.EventHandler(this.tBox_path_Enter);
+            this.tBox_path.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tBox_path_KeyDown);
+            this.tBox_path.Leave += new System.EventHandler(this.tBox_path_Leave);
+            // 
+            // btn_Detect
+            // 
+            this.btn_Detect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Detect.Location = new System.Drawing.Point(828, 12);
+            this.btn_Detect.Name = "btn_Detect";
+            this.btn_Detect.Size = new System.Drawing.Size(75, 26);
+            this.btn_Detect.TabIndex = 0;
+            this.btn_Detect.Text = "Detect";
+            this.btn_Detect.UseVisualStyleBackColor = true;
+            this.btn_Detect.Click += new System.EventHandler(this.btn_Detect_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picBox.Location = new System.Drawing.Point(12, 44);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(891, 558);
+            this.picBox.TabIndex = 2;
+            this.picBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 614);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.btn_Detect);
+            this.Controls.Add(this.tBox_path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Detect;
+        private System.Windows.Forms.TextBox tBox_path;
+        private System.Windows.Forms.PictureBox picBox;
+    }
+}
+
---
+++ ModulWrapper/ModulWrapper/Form1.cs
@@ -0,0 +1,76 @@
+﻿using Alturos.Yolo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModulWrapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string gui_default_path_text = "Enter the path to the photo and to press Enter key";
+        Color gui_default_path_color = Color.Gray;
+
+        private void tBox_path_Enter(object sender, EventArgs e)
+        {
+            if (tBox_path.Text == gui_default_path_text)
+            {
+                tBox_path.Text = null;
+                tBox_path.ForeColor = Color.Black;
+                tBox_path.Font = new Font(tBox_path.Font, FontStyle.Regular);
+            }
+        }
+
+        private void tBox_path_Leave(object sender, EventArgs e)
+        {
+            if (tBox_path.Text == "")
+            {
+                tBox_path.Text = gui_default_path_text;
+                tBox_path.ForeColor = gui_default_path_color;
+                tBox_path.Font = new Font(tBox_path.Font, FontStyle.Italic);
+            }
+        }
+
+        private void tBox_path_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                picBox.Image = Image.FromFile(tBox_path.Text);
+            }
+        }
+
+        private void btn_Detect_Click(object sender, EventArgs e)
+        {
+            using (var yoloWrapper = new YoloWrapper("Yolo-obj.cfg", "yolo-obj_1000.weights", "Anton.names"))
+            {
+                var items = yoloWrapper.Detect(@"0.Jpeg");
+                List<Alturos.Yolo.Model.YoloItem> a = new List<Alturos.Yolo.Model.YoloItem>(items);
+
+                Rectangle ee = new Rectangle(a[1].X, a[1].Y, a[1].Width, a[1].Height);
+                using (Pen pen = new Pen(Color.Red, 2))
+                {
+                    Graphics g = picBox.CreateGraphics();
+                    g.DrawRectangle(pen, ee);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picBox.Image = Image.FromFile(@"D:\_GitHubProjects\TrainCoup_project\ModulWrapper\ModulWrapper\bin\x64\Release\Img\1.jpg");
+            picBox.Image.Save(@"0.Jpeg", ImageFormat.Jpeg);
+        }
+    }
+
+}
